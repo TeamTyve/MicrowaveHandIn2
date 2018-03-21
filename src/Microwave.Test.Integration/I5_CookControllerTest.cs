2a448c2e50b6ca21942c7305a1475a26c646348a
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-03-21 21:31:22 +0100

    Adding lots of stuff

---
+++ src/Microwave.Test.Integration/I5_CookControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.Test.Integration
+{
+
+    [TestFixture]
+    class I5_CookControllerTest
+    {
+        private CookController input;
+        private Display output;
+        private ITimer timer;
+        private IPowerTube powerTube;
+        private IOutput Outputfortest;
+        private static StringWriter sw;
+        [SetUp]
+        public void Setup()
+        {
+            timer = Substitute.For<ITimer>();
+            powerTube = Substitute.For<IPowerTube>();
+            
+            Outputfortest = new Output();
+            output = new Display(Outputfortest); 
+            input = new CookController(timer,output,powerTube);
+            sw = new StringWriter();
+            Console.SetOut(sw);
+        }
+
+        [TestCase(115,1,55)]
+        [TestCase(10, 0, 10)]
+        [TestCase(0, 0, 0)]
+        public void TimerTick_DisplaysCorrectOutput(int a, int b, int c)
+        {
+            input.StartCooking(50, 60); 
+            timer.TimeRemaining.Returns(a);
+            timer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
+
+
+            sw.GetStringBuilder().Clear();
+            output.ShowTime(a, b);
+                string expected = string.Format($"Display shows: {a:D2}:{b:D2}{Environment.NewLine}");
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            
+        }
+    }
+}
---
+++ src/Microwave.Test.Integration/I7_UserInterfaceTest.cs
@@ -0,0 +1,169 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class I7_UserInterfaceTest
+    {
+        private Display output;
+        private UserInterface input;
+        private IOutput Outputfortest;
+        private IButton powerButton;
+        private IButton timeButton;
+        private IButton startCancelButton;
+        private IDoor door;
+        private ILight light;
+        private ICookController cookController;
+        private static StringWriter sw;
+        [SetUp]
+        public void Setup()
+        {
+            Outputfortest = new Output();
+            output = new Display(Outputfortest);
+            powerButton = Substitute.For<IButton>();
+            timeButton = Substitute.For<IButton>();
+            startCancelButton = Substitute.For<IButton>();
+            door = Substitute.For<IDoor>();
+            light = Substitute.For<ILight>();
+            cookController = Substitute.For<ICookController>();
+            input = new UserInterface(powerButton,
+                timeButton,
+                startCancelButton,
+                door,
+                output,
+                light,
+                cookController);
+            sw = new StringWriter();
+            Console.SetOut(sw);
+
+        }
+
+
+        [Test]
+        public void Ready_DoorOpenClose_ready_power50()
+        {
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            door.Closed += Raise.EventWith(this, EventArgs.Empty);
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            string expected = string.Format($"Display shows: 50 W{Environment.NewLine}");
+            Assert.That(expected, Is.EqualTo(sw.ToString()));
+
+        }
+
+        [Test]
+        public void Ready_powerbutton_power50()
+        {
+
+            sw.GetStringBuilder().Clear();
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            string expected = string.Format($"Display shows: 50 W{Environment.NewLine}");
+            Assert.That(expected, Is.EqualTo(sw.ToString()));
+
+        }
+
+        [Test]
+        public void Ready_21powerbutton_power50()
+        {
+
+            for (int i = 50; i <= 1000; i += 50)
+            {
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            }
+            sw.GetStringBuilder().Clear();
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            string expected = string.Format($"Display shows: 350 W{Environment.NewLine}");
+            Assert.That(expected, Is.EqualTo(sw.ToString()));
+
+        }
+
+        [Test]
+        public void CancelButton_DisplayCleared_power50()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            sw.GetStringBuilder().Clear();
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+            string expected = string.Format($"Display cleared{Environment.NewLine}");
+            Assert.That(expected, Is.EqualTo(sw.ToString()));
+
+
+        }
+
+        [Test]
+        public void Dooropened_DisplayCleared_power50()
+        {
+
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            sw.GetStringBuilder().Clear();
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+            string expected = string.Format($"Display cleared{Environment.NewLine}");
+            Assert.That(expected, Is.EqualTo(sw.ToString()));
+
+        }
+
+        [Test]
+        public void TimeButton_power50()
+        {
+
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            sw.GetStringBuilder().Clear();
+
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            string expected = string.Format($"Display shows: 01:00{Environment.NewLine}");
+            Assert.That(expected, Is.EqualTo(sw.ToString()));
+        
+        }
+
+        [Test]
+        public void TimeButton_Pressed2_power50()
+        {
+            
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+                timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            sw.GetStringBuilder().Clear();
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+                string expected = string.Format($"Display shows: 02:00{Environment.NewLine}");
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            
+        }
+
+        [Test]
+        public void DoorOpened_TimeButtonPressed_power50()
+        {
+           
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+                timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            sw.GetStringBuilder().Clear();
+            door.Opened += Raise.EventWith(this, EventArgs.Empty);
+
+                string expected = string.Format($"Display cleared{Environment.NewLine}");
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            
+        }
+
+        [Test]
+        public void Cooking_CookingDone_DisplayCleares()
+        {
+            
+                powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+                timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            sw.GetStringBuilder().Clear();
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+
+                string expected = string.Format($"Display cleared{Environment.NewLine}");
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            
+        }
+    }
+}
