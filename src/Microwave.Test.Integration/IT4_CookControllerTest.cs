735bcbc94e0ab34190f9c7d63e08fb21295bb2cd
StefanieNielson <nielson1996@hotmail.com>
2018-03-21 16:46:37 +0100

    Started testing CookController

---
+++ src/Microwave.Test.Integration/IT4_CookControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class IT4_CookControllerTest
+    {
+        //private UserInterface input;
+        private Timer outputTimer;
+        private PowerTube outputPowerTube;
+        private Output outputOutput;
+
+        private IUserInterface ui;
+
+        [SetUp]
+        public void Setup()
+        {
+            ui = Substitute.For<IUserInterface>();
+            outputTimer = new Timer();
+            outputPowerTube = new PowerTube(outputOutput);
+            outputOutput = new Output();
+
+        }
+
+        [Test]
+
+    }
+}
