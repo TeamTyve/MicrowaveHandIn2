71b7843a0d7458c381e4a39446a2853a320e4210
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-03-21 14:20:54 +0100

    Pushing

---
+++ src/Microwave.Test.Integration/I3_PowerTubeTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class I3_PowerTubeTest
+    {
+        private PowerTube input;
+        private Output output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            input = new PowerTube(output);
+            Console.WriteLine();
+        }
+
+        [TestCase(0)]
+        [TestCase(101)]
+        public void TurnOn_ThrowsException(int power)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => input.TurnOn(power));
+        }
+
+        [TestCase(1)]
+        [TestCase(100)]
+        public void TurnOn_NoThrow(int power)
+        {
+            Assert.DoesNotThrow(() => input.TurnOn(power));
+            Console.WriteLine();
+        }
+
+        [Test]
+        public void TurnOn_IsOn_ThrowsException()
+        {
+            input.TurnOn(1);
+            Assert.Throws<ApplicationException>(() => input.TurnOn(1));
+        }
+
+        [Test]
+        public void TurnOff_IsOn_OutputsTurnedOff()
+        {
+            input.TurnOn(1);
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                input.TurnOff();
+                string expected = string.Format("PowerTube turned off{0}", Environment.NewLine);
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            }
+        }
+
+    }
+}
---
+++ src/Microwave.Test.Integration/IT1_LightTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy.Internal;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Boundary;
+using NSubstitute;
+using NUnit.Framework.Internal;
+
+namespace Microwave.Test.Integration
+{
+
+   
+
+    [TestFixture]
+    public class IT1_LightTest
+    {
+        private Light input;
+        private Output output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = new Output();
+            input = new Light(output);  
+        }
+
+        [Test]
+        public void TurnOn_WasOff_CorrectOutput()
+        { 
+        
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                input.TurnOn();
+                string expected = string.Format("Light is turned on{0}", Environment.NewLine);
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            }
+
+        }
+
+        [Test]
+        public void TurnOff_WasOn_CorrectOutput()
+        {
+            input.TurnOn();
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                input.TurnOff();
+                string expected = string.Format("Light is turned off{0}", Environment.NewLine);
+                Assert.That(expected, Is.EqualTo(sw.ToString()));
+            }
+                
+        }
+
+    }
+}
